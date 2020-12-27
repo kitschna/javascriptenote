15ae4e3d861d1afdfcb0f454f247a07fbc12cbc3
kitschna <gichang.ee@gmail.com>
2020-12-28 00:25:39 +0900

    complete JS6. 12.28

---
+++ class.js
@@ -0,0 +1,126 @@
+'use strict';
+// Object-oriendted programming
+// class : template
+// object : instance of a class
+// JavaScript classes
+// - introduced in ES6
+// - syntactical sugar over prototype-based inheritance
+
+// 1. Class declarations
+class Person {
+    //onstructor
+    constructor(name, age){
+        //fields
+        this.name = name;
+        this.age = age;
+    }
+    
+    // methods
+    speak(){
+        console.log(`${this.name} : hello!`)
+    }
+}
+
+const ellie = new Person('ellie', 20)
+console.log(ellie.name)
+console.log(ellie.age)
+ellie.speak()
+
+// 2. Getter and setters
+class User {
+    constructor(firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(value) {
+        // if (value < 0){
+        //     throw Error('age can not be negative')
+        // }
+        this._age = value < 0 ? 0 : value
+    }
+}
+
+const user1 = new User('Steve', 'Job', -1)
+console.log(user1.age)
+
+// 3. Fields (public, private) // 최신 문법이라 아직 쓰기에는 무리가 있다.
+// Too soon!
+// MDN에서 찾아보기
+class Experiment {
+    publicField = 2; 
+    #privatedField = 0; // class 안에서만 쓸 수 있음
+}
+const experiment = new Experiment();
+console.log(experiment.publicField)
+console.log(experiment.privatedFiled)
+
+// 4. Static properties and methods
+// Too soon!
+class Article {
+    static publisher = 'Dream Coding';
+    constructor(articleNumber){
+        this.articleNumber = articleNumber;
+    }
+    static printPublisher(){
+        console.log(Article.publisher)
+    }
+}
+const article1 = new Article(1);
+const article2 = new Article(2);
+console.log(article1.publisher) // 안됨
+console.log(Article.publisher)
+Article.printPublisher();
+
+// 5. Inheritance
+// a way for one class to extend another class.
+class Shape {
+    constructor(width, height, color){
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw() {
+        console.log(`drawing ${this.color} color!`);
+    }
+    
+    getArea() { 
+        return this.width * this.height
+    }
+}
+
+class Rectangle extends Shape {}
+class Triangle extends Shape { // over writing 가능
+    draw(){
+        super.draw() // over writing 하면 기존의 함수는 지워지지만, 이렇게 super를 써서 부모의 것도 다시 불러올 수 있다.
+        console.log('🔺') 
+    }
+    getArea() {
+        return (this.width * this.height) /2
+    }
+
+    toString() {
+        return `Triangle color : ${this.color}`
+    }
+}
+const rectangle = new Rectangle(20, 20, 'blue')
+rectangle.draw()
+console.log('rectangle area is', rectangle.getArea())
+console.log('=========')
+const triangle = new Triangle(20, 20, 'red')
+triangle.draw()
+console.log('triangle area is', triangle.getArea())
+
+// 6. Class checking : instanceOf
+console.log(rectangle instanceof Rectangle) // T
+console.log(triangle instanceof Rectangle) // F
+console.log(triangle instanceof Triangle) // T
+console.log(triangle instanceof Shape) // T
+console.log(triangle instanceof Object) // T
+console.log(triangle.toString());
--- operator.js
+++ operator.js
@@ -32,9 +32,171 @@ const postIncrement = counter++; //(post)
 console.log(`postIncrement: ${postIncrement}, counter : ${counter}`)
 
 // 4. Assignment operators
-let x = 3;
+let x = 6;
 let y = 6;
 console.log(x += y); // x = x + y 
 console.log(x -= y); // x = x - y
 console.log(x *= y); // x = x * y
 console.log(x /= y)// x = x / y
+
+// 5. Comparison operators
+console.log(10 < 6) // less than
+console.log(10 <= 6) // less than or equal
+console.log(10 > 6) // greater than
+console.log(10 >= 6) // greatehr than
+
+// 6. Logical operators : || (or), && (and), ! (not)
+
+const value1 = true;
+const value2 = 4 < 2;
+
+// || (or), finds the first truthy value !!
+console.log(`or : ${value1 || value2 || check()}`); // 함수등은 제일 뒤에 넣는게 효율적
+// 만약 위에서 value1이 true였다면 check는 실행 하지도 않음. 이 포인트가 중요.
+
+function check() {
+    for(let i = 0; i < 10; i++){
+        //wasting time
+        console.log("🥰")
+    }
+    return true;
+}
+
+// && (and), finds the first truthy value !!
+console.log(`and : ${value1 && value2 && check()}`); // and 역시 heavy한 operation일 경우 뒤에 넣는다.
+// 만약 위에서 value1이 false라면 그대로 끝. 뒤에 있는 value 값은 볼 필요도 없음. 실행 안됨.
+
+// often used to compress long if-statement
+// nullableObject && nullableObject.something
+let nullableObject = null
+if (nullableObject != null){
+    nullableObject.something;
+}
+
+function check() {
+    for(let i = 0; i < 10; i++){
+        //wasting time
+        console.log("🥰")
+    }
+    return true;
+}
+
+// ! (not)
+console.log(!value1)
+
+// 7. Equality
+const stringFive = '5';
+const numberFive = 5;
+
+// == loose equlity, with type conversion
+console.log(stringFive == numberFive)
+console.log(stringFive != numberFive)
+console.log('vs')
+
+// === strict equality, no type conversion
+console.log(stringFive === numberFive)
+console.log(stringFive !== numberFive)
+console.log('====================')
+
+// object equality by reference
+const ellie1 = {name : 'ellie'};
+const ellie2 = {name : 'ellie'};
+const ellie3 = ellie1
+
+console.log(ellie1 == ellie2)
+console.log(ellie1 === ellie2)
+console.log(ellie1 === ellie3)
+console.log('====================')
+// 예제
+console.log('ex')
+console.log(0 == false) // 0은 falsy한 값이다.
+console.log(0 === false) // 하지만 boolean 타입은 아니다.
+console.log('' == false) // ''은 falsy한 값이다.
+console.log('' === false) // 하지만 boolean 타입은 아니다.
+console.log(null == undefined) // null은 undfined는 같은 것으로 간주되지만
+console.log(null === undefined) // 역시 boolean 타입은 아니다.
+
+
+// 8. Conditinal operator : if
+// if, else if, else
+const name ='Raf';
+if (name === 'Raf'){
+    console.log('Welcome, Raf!')
+} else if (name === 'coder') {
+    console.log('You are amaing coder')
+} else {
+    console.log('unkown')
+}
+
+// 9. Ternary operator : ?
+// condition ? value1 : value2;
+console.log(name === 'ellie' ? 'yes' : 'no')
+
+// 10. Switch statement
+// use for multiple if checks
+// use for enum-like value check
+// use for multiple type checks in TS
+
+const browser = 'IE';
+switch (browser){
+    case 'IE' :
+        console.log('go away!')
+        break;
+    case 'Chrome' :
+    case 'Firefox' :
+        console.log('I love you!');
+        break;
+    default :
+            console.log('same all!')
+        break;
+}
+
+
+// 11. Loops
+// while loop, while the condition is thrthy,
+// body code is executed
+
+let i = 3;
+while (i > 0){
+    console.log(`while : ${i}`)
+    i--;
+}
+
+// do while loop, body code is executed first, 
+// then check the condition
+
+do {
+    console.log(`do while : ${i}`)
+    i--;
+} while (i > 0)
+
+//for loop, for(begin; condition; step)
+for (i = 3; i > 0; i--){
+    console.log(`for: ${i}`)
+}
+
+
+
+// nested loops
+for (let i = 0; i < 3; i++){
+    for(let j = 0; j < 3; j++){
+        console.log(`i: ${i}, j:${j}`)
+    }
+}
+
+// break, continue
+// Q1. iterate from 0 to 10 and print only even numbers (use continue) //iterate(반복하다)
+for(let i = 0; i < 11; i++){
+    if(i % 2 !== 0){
+        continue
+    }
+    console.log(`q1. ${i}`)
+}
+
+// Q2. iterate from 0 to 10 and print numbers until reaching 8(use break)
+for (let i = 0; i < 11; i++){
+    if(i > 8){
+        break
+    }
+    console.log(`Q2. ${i}`)
+}
